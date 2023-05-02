79726ab3e1cb532e22a294699103bfd1b67cfa41
Jong-Chan Kim <jongchank@gmail.com>
2023-05-02 17:29:26 +0900

    Update hello.cu

--- hello.cu
+++ hello.cu
@@ -2,14 +2,14 @@
 
 __global__ void hello_cuda()
 {
-    printf("Hello from GPU\n");
+    printf("Hello from GPU (%d, %d)\n", blockIdx.x, threadIdx.x);
 }
 
 int main(void)
 {
     printf("Hello from CPU\n");
 
-    hello_cuda<<<1, 3>>>();
+    hello_cuda<<<2, 3>>>();
 
     cudaDeviceSynchronize();
 
